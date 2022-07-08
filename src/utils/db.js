bebab5b644ca3036865aaaa2d34546c2e5481fdf
abd <abdullahashfaq997@gmail.com>
2022-07-08 21:15:16 +0500

    fix db.js

--- src/utils/db.js
+++ src/utils/db.js
@@ -4,22 +4,26 @@ const fs = require("fs");
 require("dotenv").config();
 
 let DB = "database_test";
+let DB_USER = "postgres";
+let DB_PASSWORD = "password";
+let DB_HOST = "localhost";
 
 if (process.env.NODE_ENV === "development") {
 	DB = "database_dev";
+	DB_USER = process.env.DB_USER;
+	DB_PASSWORD = process.env.DB_PASSWORD;
+	DB_HOST = process.env.DB_HOST;
 } else if (process.env.NODE_ENV === "production") {
 	DB = "database";
+	DB_USER = process.env.DB_USER;
+	DB_PASSWORD = process.env.DB_PASSWORD;
+	DB_HOST = process.env.DB_HOST;
 }
 
-const sequelize = new Sequelize(
-	DB,
-	process.env.DB_USER,
-	process.env.PASSWD,
-	{
-		dialect: "postgres",
-		host: process.env.DB_HOST,
-		logging: false
-	}
-);
+const sequelize = new Sequelize(DB, DB_USER, DB_PASSWORD, {
+	dialect: "postgres",
+	host: DB_HOST,
+	logging: false
+});
 
 module.exports = sequelize;
